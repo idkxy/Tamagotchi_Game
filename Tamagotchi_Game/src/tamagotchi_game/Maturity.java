f358b1d24eb93fe420454ec8d66672b640678e8f
idkxy <idkxy1011@gmail.com>
2021-04-23 03:13:17 +1200

    0..0

--- Tamagotchi_Game/src/tamagotchi_game/Maturity.java
+++ Tamagotchi_Game/src/tamagotchi_game/Maturity.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -22,3 +23,56 @@ public enum Maturity {
         return maturity[random.nextInt(maturity.length)];
     }
 }
+=======
+package tamagotchi_game;
+
+import java.util.Date;
+
+/**
+ *
+ * @author daisyxiao
+ */
+public class Maturity {
+    
+    private final Pet pet;
+    private Date currentTime;
+    private long createdFor;
+    private long dInSec;
+    private long dInMin;
+    private long dInHr;
+    private long dInDay;
+    
+    public Maturity(Pet pet)
+    {
+        this.pet = pet;
+    }
+    
+    public void computeTimeDifference()
+    {
+       this.createdFor =  currentTime.getTime() - pet.getCreated().getTime();
+       this.dInSec = (createdFor / 1000) % 60;
+       this.dInMin = (createdFor / (100 * 60)) % 60;
+       this.dInHr = (createdFor / (100 * 60 * 60)) % 24;
+       this.dInDay = (createdFor / (100 * 60 * 60 * 24)) % 365;
+    }
+    
+    public void maturityCheck()
+    {
+        if(this.dInDay >= 3)
+        {
+            pet.setMaturity(true);
+        }
+        else
+        {
+            pet.setMaturity(false);
+        }
+    }
+  
+    public String toString()
+    {
+        return ("The pet has been created for: " + this.dInDay + this.dInHr + this.dInMin + this.dInSec);
+    }
+    
+    
+}
+>>>>>>> Stashed changes
--- Tamagotchi_Game/src/tamagotchi_game/Species.java
+++ Tamagotchi_Game/src/tamagotchi_game/Species.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package tamagotchi_game;
 
 import java.util.Random;
@@ -13,3 +14,16 @@ public enum Species {
         return species[random.nextInt(species.length)];
     }
 }
+=======
+package tamagotchi_game;
+
+/**
+ *
+ * @author daisyxiao
+ */
+public enum Species {
+    S1,
+    S2,
+    S3;
+}
+>>>>>>> Stashed changes
