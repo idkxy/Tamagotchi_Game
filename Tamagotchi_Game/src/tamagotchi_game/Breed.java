e8e2649e6d0969888ab3185a3871740960b7f1b8
idkxy <idkxy1011@gmail.com>
2021-04-22 16:07:53 +1200

    (˶‾᷄ ⁻̫ ‾᷅˵).

--- Tamagotchi_Game/src/tamagotchi_game/Breed.java
+++ Tamagotchi_Game/src/tamagotchi_game/Breed.java
@@ -2,11 +2,10 @@ package tamagotchi_game;
 
 import java.util.*;
 
-public class Breed {
+public class Breed extends Pet{
 
     private Pet mine;
     private Pet breed;
-    ArrayList<Pet> petList = new ArrayList<>();
     Scanner input = new Scanner(System.in);
     Random rand = new Random();
     String choice = "";
@@ -15,30 +14,34 @@ public class Breed {
     boolean run = true;
     private int petChosen;
 
+    public Breed(String name, int hunger, int thirst, int happiness, int energy) {
+        super(name, hunger, thirst, happiness, energy);
+    }
+
     public void create() {
         System.out.println("Do you wish to breed? Y/N");
         choice = input.next();
         if (choice.trim().equalsIgnoreCase("y")) {
-            if (petList.size() > 0 && petList.size() < 1) {
+            if (Pet.currentPet.childrenList.size() > 0 && childrenList.size() < 1) {
                 System.out.println("Sorry you can not breed! You need two pets to breed!");
-            } else if (petList.size() >= 2) {
+            } else if (childrenList.size() >= 2) {
                 while (run) {
                     System.out.println("Please choose from the following pets for breeding:");
-                    for (int i = 0; i < petList.size(); ++i) {
-                        System.out.println(petList.get(i + 1).toString());
+                    for (int i = 0; i < childrenList.size(); ++i) {
+                        System.out.println(childrenList.get(i + 1).toString());
                     }
                     petChosen = input.nextInt();
-                    if (!petList.get(petChosen).getName().equals(mine.getName()) ) {
-                        System.out.println("Details of the pet chosen: " + petList.get(petChosen).toString() + "Maturity stats: " + petList.get(petChosen).getMaturity());
+                    if (!childrenList.get(petChosen).getName().equals(mine.getName()) ) {
+                        System.out.println("Details of the pet chosen: " + childrenList.get(petChosen).toString() + "Maturity stats: " + childrenList.get(petChosen).getMaturity());
                         System.out.println("Are you sure you want to choose this pet for breeding? Y/N");
                         check = input.next();
                         if (choice.trim().equalsIgnoreCase("y")) {
-                            if (petList.get(petChosen).getMaturity() == Maturity.ADULT) {
+                            if (childrenList.get(petChosen).getMaturity() == Maturity.ADULT) {
                                 System.out.println("Great! A new pet has been created!");
                                 System.out.println("Please give the new pet a name: ");
                                 name = input.nextLine();
                                 breed = new Pet(name, rand.nextInt(10), rand.nextInt(10), rand.nextInt(10), rand.nextInt(10));
-                                petList.add(breed);
+                                childrenList.add(breed);
                                 System.out.println("Details of the breed: " + breed.toString());
                                 run = false;
                             } else {
