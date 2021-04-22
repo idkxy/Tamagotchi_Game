bb14fdbff622350d9d6e6cc40c0314b9b9bbb222
idkxy <idkxy1011@gmail.com>
2021-04-23 04:20:59 +1200

    Revert "0..0"
    This reverts commit f358b1d24eb93fe420454ec8d66672b640678e8f.

--- Tamagotchi_Game/src/tamagotchi_game/Shop.java
+++ Tamagotchi_Game/src/tamagotchi_game/Shop.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package tamagotchi_game;
 
 import java.util.ArrayList;
@@ -58,36 +57,3 @@ public class Shop {
     
     
 
-=======
-package tamagotchi_game;
-
-import java.util.ArrayList;
-import java.util.*;
-public class Shop {
-    
-    public Shop()
-    {
-        
-    }
-    ArrayList<Pet> pets = new ArrayList<Pet>();
-    String[] names = {"Mocha","Lucky","Ginko","Dolby","Levi"};
-    Random rand = new Random();
-    private Player p;
-    
-    public void generatePet()
-    {
-        for (int i = 0; i < 6; i++) {
-            pets.add(new Pet(names[i],rand.nextInt(10),rand.nextInt(10),rand.nextInt(10),rand.nextInt(10)));
-        }
-    }
-    
-    public void storeMenu(ArrayList<Pet> petCollection)
-    {
-        for(int i = 0; i < pets.size();++i)
-        {
-            System.out.println(pets.get(i).toString());
-        }
-    }
-    
-}
->>>>>>> Stashed changes
--- Tamagotchi_Game/src/tamagotchi_game/Species.java
+++ Tamagotchi_Game/src/tamagotchi_game/Species.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 package tamagotchi_game;
 
 import java.util.Random;
@@ -14,16 +13,3 @@ public enum Species {
         return species[random.nextInt(species.length)];
     }
 }
-=======
-package tamagotchi_game;
-
-/**
- *
- * @author daisyxiao
- */
-public enum Species {
-    S1,
-    S2,
-    S3;
-}
->>>>>>> Stashed changes
